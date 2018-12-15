6111866d99a22bed7acb372484f280baf7bdf792
Carlos Lantigua <clantigua954@gmail.com>
2018-12-15 18:59:56 -0500

    added mutator to counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,8 +2,14 @@ import React, { useContext } from 'react';
 import { CountCtx } from '../App';
 
 const Counter = () => {
-	const count = useContext(CountCtx);
-	return <div>Counter is {count}</div>;
+	const [ count, setCounter ] = useContext(CountCtx);
+	return (
+		<div>
+			<p>Counter is {count}</p>
+			<button onClick={() => setCounter(count + 5)}>+5</button>
+			<button onClick={() => setCounter(count > 0 ? count - 5 : 0)}>-5</button>
+		</div>
+	);
 };
 
 export default Counter;
